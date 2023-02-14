7c74b9fedf5e50c3f211297bbd323310acb20823
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-14 16:08:15 -0300

    feat: [Requisito 3]

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const response = await fetchProductsList('computador');
+
+console.log(response);
+
+const sectionProducts = document.getElementById('products');
+
+response.forEach((item) => sectionProducts.appendChild(createProductElement(item)));
