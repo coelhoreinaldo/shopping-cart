99a18301a92c27bfb2720afe8cec85c31a06a234
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-14 17:48:24 -0300

    feat: [Requisito 4]

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { createProductElement, createCustomElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const response = await fetchProductsList('computador');
-
-console.log(response);
-
 const sectionProducts = document.getElementById('products');
 
-response.forEach((item) => sectionProducts.appendChild(createProductElement(item)));
+const listItems = async () => {
+  const loadingSection = document.querySelector('#loading');
+  const loading = createCustomElement('p', 'loading', 'carregando...');
+  loadingSection.appendChild(loading);
+
+  const response = await fetchProductsList('computador');
+  response.forEach((item) => sectionProducts.appendChild(createProductElement(item)));
+
+  loadingSection.removeChild(loading);
+};
+
+window.onload = () => {
+  listItems();
+};
