e9bfc9c51d2ad2d0a9d450f7d27d8fca931233c7
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-15 18:28:56 -0300

    feat: [Requisito 11]

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,36 @@
-export const getAddress = () => {
-  // seu código aqui
+const errorMessage = 'CEP não encontrado';
+
+export const getAddress = async (cep) => {
+  if (!cep) {
+    throw new Error(errorMessage);
+  }
+  const response1 = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const data1 = await response1.json();
+  const response2 = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  const data2 = await response2.json();
+  const finalResponse = await Promise.any([data1, data2]);
+  return finalResponse;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cartAddressSpan = document.querySelector('.cart__address');
+  try {
+    const input = document.querySelector('.cep-input').value;
+    const {
+      address,
+      street,
+      district,
+      neighborhood,
+      state,
+      city,
+    } = await getAddress(input);
+    if (!address) {
+      throw new Error(errorMessage);
+    }
+    cartAddressSpan.innerHTML = `${address ?? street
+    } - ${district ?? neighborhood} - ${city} - ${state}`;
+  } catch (error) {
+    cartAddressSpan.innerHTML = errorMessage;
+    return error;
+  }
 };
