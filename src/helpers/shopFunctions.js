5a67235fb5faa650e5f42e196ffd98b44df8f21b
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-15 10:14:09 -0300

    feat: [Requisito 8]

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -122,6 +123,13 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'Adicionar ao carrinho!',
   );
   section.appendChild(cartButton);
-
+  cartButton.addEventListener('click', async () => {
+    const itemId = cartButton.parentNode.firstChild.innerText;
+    saveCartID(itemId);
+    const productData = await fetchProduct(itemId);
+    const productDom = createCartProductElement(productData);
+    const olCartList = document.querySelector('.cart__products');
+    olCartList.appendChild(productDom);
+  });
   return section;
 };
